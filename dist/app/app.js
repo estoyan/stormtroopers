96f1ceac4429a52d6a26365317ca6c8b2a53252b
george-georgiev <george.georgiev@mail.bg>
2016-12-22 17:34:49 +0200

    Remove dist from root folder

--- dist/app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var config = require('./config')();
-var app = require('./config/application')();
-var data = require('./data')({ config: config });
-var controllers = require('./controllers')({ data: data });
-require('./routes')({ app: app, data: data, controllers: controllers });
-app.listen(config.port, function () { return console.log("May the force be with you on: " + config.port); });
-//# sourceMappingURL=app.js.map
--- dist/app/public/app/shared/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var star_1 = require('./star');
-exports.AcStar = star_1.AcStar;
-var stars_1 = require('./stars');
-exports.AcStars = stars_1.AcStars;
-//# sourceMappingURL=index.js.map
