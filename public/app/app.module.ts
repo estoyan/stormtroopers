06ff666bd853a2177b908b01f1e8659526b67294
Yankov <stoian_y@yahoo.com>
2016-12-26 16:57:41 +0200

    Merge fixed

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
@@ -10,17 +9,7 @@ import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppRoutingModule } from './routing/app-routing.module';
 import { CoreModule } from './core/core.module';
-<<<<<<< HEAD
-
-//  Pages
-import { CharacterComponent } from './pages/characters/cahracter.component';
-import { PageNotFoundComponent } from './pages/page-not-fount/page-not-fount.component';
-import { HomeComponent } from './pages/home/home.component';
-// import { MoviesComponent } from './pages/movies/movies.component';
-import { LoginComponent } from './pages/login/login.component';
-=======
 import { PageLoaderModule } from './pages/page-loader.module';
->>>>>>> 853a409d5d2438113ddf20d92e6359a465328d55
 import { RegisterComponent } from './pages/register/register.component';
 
 //  Services
@@ -44,14 +33,6 @@ import { AuthGuard } from './guards/authGuard';
   ],
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CharacterComponent,
-    HomeComponent,
-    // MoviesComponent,
-    PageNotFoundComponent,
-    LoginComponent,
-=======
->>>>>>> 853a409d5d2438113ddf20d92e6359a465328d55
     RegisterComponent,
   ],
   bootstrap: [AppComponent],
