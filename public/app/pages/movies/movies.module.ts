562643ffe338478e66a8ccc57c5798cb22375695
Yankov <stoian_y@yahoo.com>
2016-12-31 13:44:08 +0200

    Movie-List added pagination Filter/Sort Pipe issue fixed

--- public/app/pages/movies/movies.module.ts
+++ public/app/pages/movies/movies.module.ts
@@ -1,15 +1,17 @@
 import { NgModule, ApplicationModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+ import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { MovieRoutingModule, routedComponents } from './movies-routing.module';
-// import { InfiniteScrollModule } from 'angular2-infinite-scroll';
-// import { InfiniteScrollModule } from '../../../../node_modules/angular2-infinite-scroll/angular2-infinite-scroll.d';
 import { MovieComponent } from './movie/movie.component'
 import { MovieListComponent } from './movie-list/movie-list.component'
+
 import { MovieService } from '../../services/movies/movie.service';
 
 @NgModule({
-  imports: [MovieRoutingModule, CommonModule],
+  // ,PaginationModule.forRoot()
+  imports: [MovieRoutingModule,FormsModule, CommonModule,PaginationModule.forRoot()],
   declarations: [routedComponents, MovieComponent, MovieListComponent],
   providers: [MovieService]
 })
