496c76d1d46af75de8467ffffde81800ebfcefab
Yankov <stoian_y@yahoo.com>
2017-01-02 18:22:10 +0200

    Publication Filter and Search added Product Pagination Added

--- public/app/pages/publications/publications.module.ts
+++ public/app/pages/publications/publications.module.ts
@@ -3,24 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { PublicationsRoutingModule, routedComponents } from './publications-routing.module';
-import {PublicationsOverviewModule} from '../../shared/publication-overview/publications-overview.module';
+import { PublicationsOverviewModule } from '../../shared/publication-overview/publications-overview.module';
 import { PipeModule } from '../../pipes/pipe.module';
-import {StarsModule} from '../../shared/stars/stars.module';
+import { StarsModule } from '../../shared/stars/stars.module';
 import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { FilterModule } from '../../shared/filter/filter.module';
+import { SortModule } from '../../shared/sort/sort.module';
 
 
 import { PublicatonsService } from '../../services/publications/publications.service';
 
 @NgModule({
     imports: [
-        FormsModule, 
-    CommonModule, 
-    PublicationsRoutingModule, 
-    PublicationsOverviewModule,
-     PipeModule, 
-     StarsModule,
-     PaginationModule.forRoot()
-     ],
+        FormsModule,
+        CommonModule,
+        FilterModule,
+        SortModule,
+        PublicationsRoutingModule,
+        PublicationsOverviewModule,
+        PipeModule,
+        StarsModule,
+        PaginationModule.forRoot()
+    ],
     declarations: [routedComponents],
     providers: [PublicatonsService]
 })
